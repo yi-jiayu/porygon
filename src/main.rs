fdc52bfbef6e5961e61f05e0aa9a677b934dfeae
Jiayu Yi <yijiayu@gmail.com>
2020-01-04 11:40:16 +0800

    Add new event endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,30 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
-#[macro_use] extern crate rocket;
+#[macro_use]
+extern crate rocket;
+
+use rocket_contrib::json::Json;
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+struct Event {
+    chat_id: i64,
+    user_id: i64,
+}
 
 #[get("/")]
 fn index() -> &'static str {
     "Hello, world!"
 }
 
+#[post("/", format = "json", data = "<event>")]
+fn new_event(event: Json<Event>) -> Json<Event> {
+    return event;
+}
+
 fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
+    rocket::ignite()
+        .mount("/", routes![index])
+        .mount("/events", routes![new_event])
+        .launch();
 }
