4f6840d569e28c259234eaf08eb7f680663f9c7d
Jiayu Yi <yijiayu@gmail.com>
2020-01-04 23:18:20 +0800

    Output collector and Metabase URLs

--- deploy/outputs.tf
+++ deploy/outputs.tf
@@ -1,3 +1,7 @@
-output "url" {
+output "collector_url" {
   value = google_cloud_run_service.porygon.status[0].url
+}
+
+output "metabase_url" {
+  value = "http://${google_compute_instance.metabase.network_interface.0.access_config.0.nat_ip}:8080"
 }
